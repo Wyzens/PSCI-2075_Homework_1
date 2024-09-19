3d72a24b09e169cc985f3c0e795aad2762b6db7f
Endre Kereshi <ekereshi@gmail.com>
2024-09-19 00:44:29 -0600

    cleaned up code, added comments, fixed war data analysis

--- Analysis.R
+++ Analysis.R
@@ -1,37 +1,45 @@
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#Contributors: Endre Kereshi, Jack F, Maggie Rich, Venera Aslanian
 
 library(RCPA3)
 library(foreign)
 
-View ("RCPA3")
+# View the RCPA3 dataset
+View(nes)
 
-nrow(nes)
-nrow(states)
-nrow(world)
+# Get the number of observations in each dataset
+nrow(nes)  # Number of observations in nes
+nrow(states)  # Number of observations in states
+nrow(world)  # Number of observations in world
 
+# View the states dataset
 View(states)
 
+# Summary of the cook.index3 variable
 summary(states$cook.index3)
 
+# Find max and min values for the abortlaws variable
 max(states$abortlaws)
 min(states$abortlaws)
 
+# Create a histogram of the abortlaws variable
 hist(states$abortlaws)
 
-View("interstate_wars_data")
+# View the interstate wars data
+View(interstate_wars_data)
 
+# Find max and min values for the deaths variable
 max(interstate_wars_data$deaths)
 min(interstate_wars_data$deaths)
 
+# Create a histogram of the deaths variable
 hist(interstate_wars_data$deaths)
 
-interstate_wars_data$totallength <- interstate_wars_data$length1 +interstate_wars_data$length2 + interstate_wars_data$trucelength
+# Calculate total length of wars
+interstate_wars_data$totallength <- interstate_wars_data$length1 + interstate_wars_data$length2 + interstate_wars_data$trucelength
 
+# Find max and min values for the total length of wars
 max(interstate_wars_data$totallength)
 min(interstate_wars_data$totallength)
 
+# Create a histogram of the total length of wars
 hist(interstate_wars_data$totallength)
